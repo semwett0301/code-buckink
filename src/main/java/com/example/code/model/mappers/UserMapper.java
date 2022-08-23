fe5a268d7998af8933afe9607d8c41ec2d2dc2e2
sem_wett <90400355+semwett0301@users.noreply.github.com>
2022-08-23 11:14:33 +0300

    Change pom.xml and add system.properties

--- src/main/java/com/example/code/model/mappers/UserMapper.java
+++ src/main/java/com/example/code/model/mappers/UserMapper.java
@@ -2,8 +2,10 @@ package com.example.code.model.mappers;
 
 import com.example.code.model.dto.ResponseUserDTO;
 import com.example.code.model.entities.UserInfo;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
